b3c9e395ddd1fddd213557e1cf957a2e2462fed1
Leslie <lesbellamy99@gmail.com>
2023-12-15 20:55:59 -0500

    update

--- Basket/Present/Western/Western.js
+++ Basket/Present/Western/Western.js
@@ -30,7 +30,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/dallas.gif";
+
+        MainFetchPopUp("/basket/present/western/json/dallas.json");
     }
 
     if (selectedYear === "1") {
@@ -47,7 +49,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/denver.gif";
+
+        MainFetchPopUp("/basket/present/western/json/denver.json");
     }
 
     if (selectedYear === "2") {
@@ -64,7 +68,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/golden state.gif";
+
+        MainFetchPopUp("/basket/present/western/json/golden state.json");
     }
 
     if (selectedYear === "3") {
@@ -80,7 +86,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/houston.gif";
+
+        MainFetchPopUp("/basket/present/western/json/houston.json");
     }
 
     if (selectedYear === "4") {
@@ -97,7 +105,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/la clippers.gif";
+
+        MainFetchPopUp("/basket/present/western/json/la clippers.json");
     }
 
     if (selectedYear === "5") {
@@ -113,7 +123,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/la lakers.gif";
+
+        MainFetchPopUp("/basket/present/western/json/la lakers.json");
     }
 
     if (selectedYear === "6") {
@@ -129,7 +141,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/memphis.gif";
+
+        MainFetchPopUp("/basket/present/western/json/memphis.json");
     }
 
     if (selectedYear === "7") {
@@ -146,7 +160,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/minnesota.gif";
+
+        MainFetchPopUp("/basket/present/western/json/minnesota.json");
     }
 
     if (selectedYear === "8") {
@@ -163,7 +179,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/new orleans.gif";
+
+        MainFetchPopUp("/basket/present/western/json/new orleans.json");
     }
 
     if (selectedYear === "9") {
@@ -179,7 +197,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/oklahoma city.gif";
+
+        MainFetchPopUp("/basket/present/western/json/oklahoma city.json");
     }
 
     if (selectedYear === "10") {
@@ -195,7 +215,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/phoenix.gif";
+
+        MainFetchPopUp("/basket/present/western/json/phoenix.json");
     }
 
     if (selectedYear === "11") {
@@ -212,7 +234,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/portland.gif";
+
+        MainFetchPopUp("/basket/present/western/json/portland.json");
     }
 
     if (selectedYear === "12") {
@@ -229,7 +253,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/sacramento.gif";
+
+        MainFetchPopUp("/basket/present/western/json/sacramento.json");
     }
 
     if (selectedYear === "13") {
@@ -246,7 +272,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/san antonio.gif";
+
+        MainFetchPopUp("/basket/present/western/json/san antonio.json");
     }
 
     if (selectedYear === "14") {
@@ -262,7 +290,9 @@ function updatePlayerInfo() {
         PF2.src = "/basket/img/players/" + PF.textContent + ".png";
         C2.src = "/basket/img/players/" + C.textContent + ".png";
 
-        smallpic.src = "";
+        smallpic.src = "/basket/img/logos/utah.gif";
+
+        MainFetchPopUp("/basket/present/western/json/utah.json");
     }
 
 
@@ -306,7 +336,283 @@ updatePlayerInfo();
     
 const updateDropdown = () => {
 
-    
+    if (selectedYear === 0) {
+        PG.textContent = "L Doncic";
+        SG.textContent = "K Irving";
+        SF.textContent = "T Hardaway Jr";
+        PF.textContent = "G Williams";
+        C.textContent = "D Lively II";
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/dallas.gif";
+
+        MainFetchPopUp("/basket/present/western/json/dallas.json");
+    }
+
+    if (selectedYear === 1) {
+        PG.textContent = "J Murray";
+        SG.textContent = "K Caldwell-Pope";
+        SF.textContent = "M Porter Jr";
+        PF.textContent = "A Gordon";
+        C.textContent = "N Jokic";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/denver.gif";
+
+        MainFetchPopUp("/basket/present/western/json/denver.json");
+    }
+
+    if (selectedYear === 2) {
+        PG.textContent = "C Paul";
+        SG.textContent = "S Curry";
+        SF.textContent = "K Thompson";
+        PF.textContent = "A Wiggins";
+        C.textContent = "D Green";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/golden state.gif";
+
+        MainFetchPopUp("/basket/present/western/json/golden state.json");
+    }
+
+    if (selectedYear === 3) {
+        PG.textContent = "F VanVleet";
+        SG.textContent = "J Green";
+        SF.textContent = "D Brooks";
+        PF.textContent = "J Smith Jr";
+        C.textContent = "A Sengun";
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/houston.gif";
+
+        MainFetchPopUp("/basket/present/western/json/houston.json");
+    }
+
+    if (selectedYear === 4) {
+        PG.textContent = "J Harden";
+        SG.textContent = "R Westbrook";
+        SF.textContent = "P George";
+        PF.textContent = "K Leonard";
+        C.textContent = "I Zubac";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/la clippers.gif";
+
+        MainFetchPopUp("/basket/present/western/json/la clippers.json");
+    }
+
+    if (selectedYear === 5) {
+        PG.textContent = "D Russell";
+        SG.textContent = "A Reaves";
+        SF.textContent = "L James";
+        PF.textContent = "R Hachimura";
+        C.textContent = "A Davis";
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/la lakers.gif";
+
+        MainFetchPopUp("/basket/present/western/json/la lakers.json");
+    }
+
+    if (selectedYear === 6) {
+        PG.textContent = "J Morant";
+        SG.textContent = "M Smart";
+        SF.textContent = "D Bane";
+        PF.textContent = "J Jackson Jr";
+        C.textContent = "S Adams";
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/memphis.gif";
+
+        MainFetchPopUp("/basket/present/western/json/memphis.json");
+    }
+
+    if (selectedYear === 7) {
+        PG.textContent = "A Edwards";
+        SG.textContent = "J McDaniels";
+        SF.textContent = "N Reid";
+        PF.textContent = "KA Towns";
+        C.textContent = "R Gobert";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/minnesota.gif";
+
+        MainFetchPopUp("/basket/present/western/json/minnesota.json");
+    }
+
+    if (selectedYear === 8) {
+        PG.textContent = "CJ McCollum";
+        SG.textContent = "H Jones";
+        SF.textContent = "B Ingram";
+        PF.textContent = "Z Williamson";
+        C.textContent = "J Valanciunas";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/new orleans.gif";
+
+        MainFetchPopUp("/basket/present/western/json/new orleans.json");
+    }
+
+    if (selectedYear === 9) {
+        PG.textContent = "S Gilgeous-Alexander";
+        SG.textContent = "J Giddey";
+        SF.textContent = "J Williams";
+        PF.textContent = "L Dort";
+        C.textContent = "C Holmgren";
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/oklahoma city.gif";
+
+        MainFetchPopUp("/basket/present/western/json/oklahoma city.json");
+    }
+
+    if (selectedYear === 10) {
+        PG.textContent = "D Booker";
+        SG.textContent = "B Beal";
+        SF.textContent = "E Gordon";
+        PF.textContent = "K Durant";
+        C.textContent = "J Nurkic";
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/phoenix.gif";
+
+        MainFetchPopUp("/basket/present/western/json/phoenix.json");
+    }
+
+    if (selectedYear === 11) {
+        PG.textContent = "M Brogdon";
+        SG.textContent = "A Simons";
+        SF.textContent = "S Sharpe";
+        PF.textContent = "D Ayton";
+        C.textContent = "R Williams III";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/portland.gif";
+
+        MainFetchPopUp("/basket/present/western/json/portland.json");
+    }
+
+    if (selectedYear === 12) {
+        PG.textContent = "D Fox";
+        SG.textContent = "M Monk";
+        SF.textContent = "H Barnes";
+        PF.textContent = "K Murray";
+        C.textContent = "D Sabonis";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/sacramento.gif";
+
+        MainFetchPopUp("/basket/present/western/json/sacramento.json");
+    }
+
+    if (selectedYear === 13) {
+        PG.textContent = "V Wembanyama";
+        SG.textContent = "Tr Jones";
+        SF.textContent = "K Johnson";
+        PF.textContent = "D Vassell";
+        C.textContent = "Z Collins";
+
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/san antonio.gif";
+
+        MainFetchPopUp("/basket/present/western/json/san antonio.json");
+    }
+
+    if (selectedYear === 14) {
+        PG.textContent = "C Sexton";
+        SG.textContent = "J Clarkson";
+        SF.textContent = "L Markkanen";
+        PF.textContent = "J Collins";
+        C.textContent = "W Kessler";
+
+        PG2.src = "/basket/img/players/" + PG.textContent + ".png";
+        SG2.src = "/basket/img/players/" + SG.textContent + ".png";
+        SF2.src = "/basket/img/players/" + SF.textContent + ".png";
+        PF2.src = "/basket/img/players/" + PF.textContent + ".png";
+        C2.src = "/basket/img/players/" + C.textContent + ".png";
+
+        smallpic.src = "/basket/img/logos/utah.gif";
+
+        MainFetchPopUp("/basket/present/western/json/utah.json");
+    }
 
     function clearAndFadeInElements() {
         const elements = [
