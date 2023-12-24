d455e9cdb14ad1fb8b1e940461d7d8304e35fb3b
Leslie <lesbellamy99@gmail.com>
2023-12-23 22:19:53 -0500

    update

--- Football/Search.js
+++ Football/Search.js
@@ -1,7 +1,7 @@
 const description = document.getElementById("desc");
 let allPlayers = [];
 let currentPage = 1;
-const itemsPerPage = 20;
+const itemsPerPage = 48;
 let includeRetired = true;
 
 // Function to load JSON data from a file
