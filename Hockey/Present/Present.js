5d9ce599a2fdb323a62b771fd013d49e228c1758
Leslie <lesbellamy99@gmail.com>
2024-01-29 10:49:00 -0500

    update

--- Hockey/Present/Present.js
+++ Hockey/Present/Present.js
@@ -65,12 +65,12 @@ function updatePlayerInfo() {
     DR.textContent = "Josi";
     G.textContent = "Vasilevskiy";
 
-    FL2.src = "/hockey/img/players/" + FL.textContent + ".jpg";
-    FC2.src = "/hockey/img/players/" + FC.textContent + ".jpg";
-    FR2.src = "/hockey/img/players/" + FR.textContent + ".jpg";
-    DL2.src = "/hockey/img/players/" + DL.textContent + ".jpg";
-    DR2.src = "/hockey/img/players/" + DR.textContent + ".jpg";
-    G2.src = "/hockey/img/players/" + G.textContent + ".jpg";
+    FL2.src = "/Hockey/img/players/" + FL.textContent + ".jpg";
+    FC2.src = "/Hockey/img/players/" + FC.textContent + ".jpg";
+    FR2.src = "/Hockey/img/players/" + FR.textContent + ".jpg";
+    DL2.src = "/Hockey/img/players/" + DL.textContent + ".jpg";
+    DR2.src = "/Hockey/img/players/" + DR.textContent + ".jpg";
+    G2.src = "/Hockey/img/players/" + G.textContent + ".jpg";
 
     smallpic.src = "/hockey/img/wallpapers/mcdavid.jpg";
 
