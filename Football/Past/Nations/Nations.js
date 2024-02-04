641452697ba7dd3d2f3ce4839a3c3caae246d798
Leslie <lesbellamy99@gmail.com>
2024-02-04 10:29:10 -0500

    update

--- Football/Past/Nations/Nations.js
+++ Football/Past/Nations/Nations.js
@@ -108,7 +108,7 @@ function updatePlayerInfo() {
     if (selectedYear == "4") {
         STC.textContent = "Benzema";
         FR.textContent = "Ribery";
-        AMC.textContent = "Griezmann10";
+        AMC.textContent = "Griezmann";
         FL.textContent = "Mbappe";
         CMR.textContent = "Pogba";
         CML.textContent = "Kante";
