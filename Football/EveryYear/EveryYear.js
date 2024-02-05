0d72c7348732a5d4f8a711bb8b4c76b4f0327ab9
Leslie <lesbellamy99@gmail.com>
2024-02-05 08:22:39 -0500

    auto deploy

--- Football/EveryYear/EveryYear.js
+++ Football/EveryYear/EveryYear.js
@@ -108,7 +108,7 @@ function updatePlayerInfo() {
         STC.textContent = "Henry";
         FR.textContent = "C Ronaldo";
         AMC.textContent = "Zidane";
-        FL.textContent = "Ronaldinho17";
+        FL.textContent = "Ronaldinho";
         CMR.textContent = "Lampard";
         CML.textContent = "Pirlo";
         DR.textContent = "Cafu";
